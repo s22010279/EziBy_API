fa7a8f4fea39272f53799a38732d3b1079ae9872
thamuditha\Thamuditha <s22010180@ousl.lk>
2024-07-14 12:07:14 +0530

    created brand.cs

---
+++ EziBy_Core_ClassLibrary/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandImage { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool Active { get; set; }
+        public bool DisplayableInMobileApp { get; set; }
+    }
+}
