2938d7c4ec7301747f72af6b93593d3457d693af
DESKTOP-V9GBM3R\user <s22010367@ousl.lk>
2024-07-14 22:00:11 +0530

    added model "customerPaymentSub"

---
+++ EziBy_Core_ClassLibrary/Models/CustomerPaymentSub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EziBy_Core_ClassLibrary.Models
+{
+    internal class CustomerPaymentSub
+    {
+        public int CustomerPaymentSubId { get; set; }//auto number
+        public int CustomerPaymentMainId { get; set; }//ref
+        public int PosMainId { get; set; }//ref
+        public decimal SettledAmount { get; set; }
+    }
+}
