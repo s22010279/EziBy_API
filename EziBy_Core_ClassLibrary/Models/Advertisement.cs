0101f0fb3d8c8ff4c6613be747cc8117f9e9ccf5
S22010177 - Rashmika Rodrigo <S22010177@ousl.lk>
2024-07-14 12:24:49 +0530

    added advertisement

---
+++ EziBy_Core_ClassLibrary/Models/Advertisement.cs
@@ -0,0 +1,16 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public partial class Advertisement
+    {
+        public int AdvertisementId { get; set; }
+        public string AdvertisementImage { get; set; }
+        public string AdvertisementLink { get; set; }
+        public string CompanyName { get; set; }
+        public string PhoneNumber1 { get; set; }
+        public string PhoneNumber2 { get; set; }
+        public string Slogan { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool Paid { get; set; }
+        public bool Active { get; set; }
+    }
+}
