d53bf2c3878725c8943b085652fe88e082a183dc
DESKTOP-V9GBM3R\user <s22010367@ousl.lk>
2024-07-14 22:06:22 +0530

    re-corrected the code

--- EziBy_Core_ClassLibrary/Models/CustomerPaymentSub.cs
+++ EziBy_Core_ClassLibrary/Models/CustomerPaymentSub.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EziBy_Core_ClassLibrary.Models
+﻿namespace EziBy_Core_ClassLibrary.Models
 {
     internal class CustomerPaymentSub
     {
