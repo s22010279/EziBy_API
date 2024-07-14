ee60957052f09f1908cd322a4e039993347ccadf
S22010177 - Rashmika Rodrigo <S22010177@ousl.lk>
2024-07-14 22:11:25 +0530

    resolved errors in PosMain

--- EziBy_Core_ClassLibrary/Models/PosMain.cs
+++ EziBy_Core_ClassLibrary/Models/PosMain.cs
@@ -5,12 +5,9 @@
         public int PosMainId { get; set; }
         public string PosGUID { get; set; }
         public DateTime PosDate { get; set; }
-        public int CustomerId { get; set; }//ref
+        public int CustomerId { get; set; }
         public bool Cancelled { get; set; }
         public decimal GrossAmount { get; set; }
-        //public decimal DiscountPercent { get; set; }
-        //public decimal DiscountAmount { get; set; }
-        //public decimal DeductionAmount { get; set; }
         public decimal PermissableFraction { get; set; }
         public decimal NetAmount { get; set; }
         public decimal CashAmount { get; set; }
@@ -24,20 +21,9 @@
         public DateTime? ChequeValueDate { get; set; }
         public decimal CreditAmount { get; set; }
         public decimal BalanceAmount { get; set; }
-        //public byte[] QRCode { get; set; }
-
-        public int Prepared_UserId { get; set; }//ref
+        public int Prepared_UserId { get; set; }
         public DateTime Prepared_Date { get; set; }
-        public int Modified_UserId { get; set; }//ref
+        public int Modified_UserId { get; set; }
         public DateTime Modified_Date { get; set; }
-
-        //references
-        public virtual Customer Customer { get; set; }
-        public virtual User User { get; set; }
-        public virtual User User1 { get; set; } //for 2nd foreign key ref
-        public virtual ICollection<PosSub> PosSubs { get; set; }
-        public virtual ICollection<PosReturnMain> PosReturnMains { get; set; }
-        public virtual ICollection<PosReturnSub> PosReturnSubs { get; set; }
-        public virtual ICollection<CustomerPaymentSub> CustomerPaymentSubs { get; set; }
     }
 }
