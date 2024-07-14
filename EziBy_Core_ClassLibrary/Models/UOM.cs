d7e89fd6996e65bd339e795eab93b0ec25ec3fac
DESKTOP-V9GBM3R\user <s22010367@ousl.lk>
2024-07-14 11:54:50 +0530

    replaced " " with a "-"

--- EziBy_Core_ClassLibrary/Models/UOM.cs
+++ EziBy_Core_ClassLibrary/Models/UOM.cs
@@ -5,7 +5,7 @@
         public int UOMId { get; set; }
         public string UOMName { get; set; }
         public string UOMFullName { get; set; }
-        public string UOMNameWithFullName => UOMName + " " + UOMFullName;
+        public string UOMNameWithFullName => UOMName + "-" + UOMFullName;
         public bool Active { get; set; }
     }
 }
