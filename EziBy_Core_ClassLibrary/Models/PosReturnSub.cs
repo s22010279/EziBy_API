6e05f2c89059c7421138807ddc15eb3a842de9f6
S22010177 - Rashmika Rodrigo <S22010177@ousl.lk>
2024-07-15 14:36:10 +0530

    added POS return sub model

---
+++ EziBy_Core_ClassLibrary/Models/PosReturnSub.cs
@@ -0,0 +1,12 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public class PosReturnSub
+    {
+        public int PosReturnSubId { get; set; }
+        public int PosReturnMainId { get; set; }
+        public int PosMainId { get; set; }
+        public int PosSubId { get; set; }
+        public decimal ReturnedQty { get; set; }
+        public decimal DiscountedSellingPrice { get; set; }
+    }
+}
