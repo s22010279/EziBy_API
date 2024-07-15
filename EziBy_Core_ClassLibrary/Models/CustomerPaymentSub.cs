09b08ffc257adf732a035adb57e6fc07a14ac51e
M.Z.M Anushath <s22010279@ousl.lk>
2024-07-15 10:44:51 +0530

    fixed the spelling errors

--- EziBy_Core_ClassLibrary/Models/CustomerPaymentSub.cs
+++ EziBy_Core_ClassLibrary/Models/CustomerPaymentSub.cs
@@ -5,6 +5,6 @@
         public int CustomerPaymentSubId { get; set; }//auto number
         public int CustomerPaymentMainId { get; set; }//ref
         public int PosMainId { get; set; }//ref
-        public decimal SettledAmount { get; set11; }
+        public decimal SettledAmount { get; set; }
     }
 }
