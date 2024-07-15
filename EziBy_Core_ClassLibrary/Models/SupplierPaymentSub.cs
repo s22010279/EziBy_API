716cb54fc9774dc5c9bed90029f69dd8a5631ffe
thamuditha\Thamuditha <s22010180@ousl.lk>
2024-07-15 12:54:31 +0530

    created SupplierPaymentSub model

---
+++ EziBy_Core_ClassLibrary/Models/SupplierPaymentSub.cs
@@ -0,0 +1,10 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public class SupplierPaymentSub
+    {
+        public int SupplierPaymentSubId { get; set; }
+        public int SupplierPaymentMainId { get; set; }
+        public int GrnMainId { get; set; }
+        public decimal SettledAmount { get; set; }
+    }
+}
