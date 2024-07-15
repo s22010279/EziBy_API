a37dc828f867ee67c01edc0ac92f848d9ad5a89c
DESKTOP-V9GBM3R\user <s22010367@ousl.lk>
2024-07-15 17:09:05 +0530

    added model "MobileOrderSub"

---
+++ EziBy_Core_ClassLibrary/Models/MobileOrderSub.cs
@@ -0,0 +1,16 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public partial class MobileOrderSub
+    {
+        public int MobileOrderSubId { get; set; }
+        public string MobileOrderMainId { get; set; }//ref
+        public int ItemId { get; set; }//ref
+        public int ItemPriceVariantId { get; set; }//ref
+        public int Quantity { get; set; }
+        public decimal SellingPrice { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal DiscountedSellingPrice { get; set; }
+        public int OrderStatusId { get; set; } // for counting the remaining quantity if rejected it will not add to sum of sold quantity
+    }
+}
