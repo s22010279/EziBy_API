ddc7f725bb4eae2e5df39d905aa79c91b1f56ad7
DESKTOP-V9GBM3R\user <s22010367@ousl.lk>
2024-07-15 08:56:59 +0530

    added model "SubCatergory"

---
+++ EziBy_Core_ClassLibrary/Models/SubCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public partial class SubCategory
+    {
+        public int SubCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        public string SubCategoryName { get; set; }
+        public string SubCategoryImage { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool Active { get; set; }
+    }
+}
