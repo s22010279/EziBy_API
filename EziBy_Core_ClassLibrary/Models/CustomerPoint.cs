938d12e92621da210844410ec44e08dc01c6bd76
M.Z.M Anushath <s22010279@ousl.lk>
2024-07-16 08:34:28 +0530

    added CustomerPointRedeemp model

---
+++ EziBy_Core_ClassLibrary/Models/CustomerPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EziBy_Core_ClassLibrary.Models
+{
+    public class CustomerPointRedeemp
+    {
+        public int RedeepmedId { get; set; }
+        public int CustomerId { get; set; }
+        public int PointsRedeemped { get; set; }
+        public DateTime RedeepedDate { get; set; }
+
+    }
+}
