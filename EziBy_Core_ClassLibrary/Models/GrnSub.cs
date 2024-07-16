1f7150cca2b10da11e536a9ea8b79c730162088f
M.Z.M Anushath <s22010279@ousl.lk>
2024-07-16 08:13:35 +0530

    added GrnSub model

---
+++ EziBy_Core_ClassLibrary/Models/GrnSub.cs
@@ -0,0 +1,13 @@
+﻿namespace EziBy_Core_ClassLibrary.Models
+{
+    public class GrnSub
+    {
+        public int GrnSubId { get; set; }
+        public int GrnMainId { get; set; }//ref
+
+        public int ItemId { get; set; }//ref
+        public int ItemPriceVariantId { get; set; }//ref
+        public decimal CostPrice { get; set; }
+        public decimal GrnQty { get; set; }
+    }
+}
